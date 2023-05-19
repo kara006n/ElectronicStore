ce8a934d56ff2c4d5b0ef5ce85ba8226c38bbb4e
Neel Kamal <kara006n@gmail.com>
2023-05-19 13:31:16 +0530

    Two classes added for handling Custom Exceptions

---
+++ ElectronicStore/ElectronicStore/src/main/java/com/project/ElectronicStore/exception/ResourceNotFoundException.java
@@ -0,0 +1,15 @@
+package com.project.ElectronicStore.exception;
+
+import lombok.Builder;
+
+@Builder
+public class ResourceNotFoundException extends RuntimeException{
+
+      public ResourceNotFoundException(){
+            super("Resource not found");
+      }
+
+      public ResourceNotFoundException(String message){
+            super(message);
+      }
+}
