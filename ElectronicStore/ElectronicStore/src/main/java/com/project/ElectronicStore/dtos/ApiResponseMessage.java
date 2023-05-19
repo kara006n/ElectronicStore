d24c8f6d4d26057d1b53db8662480d0f3cec97b5
Neel Kamal <kara006n@gmail.com>
2023-05-19 13:00:54 +0530

    ApiResponseMessage class is created for custom response messages during api interaction

---
+++ ElectronicStore/ElectronicStore/src/main/java/com/project/ElectronicStore/dtos/ApiResponseMessage.java
@@ -0,0 +1,16 @@
+package com.project.ElectronicStore.dtos;
+
+import lombok.*;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ApiResponseMessage {
+
+      private String message;
+      private boolean success;
+      private HttpStatus status;
+}
